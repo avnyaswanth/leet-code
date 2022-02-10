64854301fe48e12b2a59685bba125da77bfd8f1e
avnyaswanth <68109893+avnyaswanth@users.noreply.github.com>
2022-02-10 15:00:24 +0530

    Time: 56 ms (80.07%), Space: 13.6 MB (33.24%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -1,81 +1,53 @@
 class Solution {
 public:
-    // vector<vector<int>> fourSum(vector<int>& nums, int target) {
-    //     sort(begin(nums), end(nums));
-    //     return kSum(nums, target, 0, 4);
-    // }
-	
-    vector<vector<int>> fourSum(vector<int>& num, int target) {
-        vector<vector<int> > res;
-        
-        if (num.empty())
-            return res;
-        int n = num.size(); 
-        sort(num.begin(),num.end());
-    
-        for (int i = 0; i < n; i++) {
-        
-            int target_3 = target - num[i];
-        
-            for (int j = i + 1; j < n; j++) {
-            
-                int target_2 = target_3 - num[j];
-            
-                int front = j + 1;
-                int back = n - 1;
-            
-                while(front < back) {
-                
-                    int two_sum = num[front] + num[back];
-                
-                    if (two_sum < target_2) front++;
-                
-                    else if (two_sum > target_2) back--;
-                
-                    else {
-                    
-                        vector<int> quadruplet(4, 0);
-                        quadruplet[0] = num[i];
-                        quadruplet[1] = num[j];
-                        quadruplet[2] = num[front];
-                        quadruplet[3] = num[back];
-                        res.push_back(quadruplet);
-                    
-                        // Processing the duplicates of number 3
-                        while (front < back && num[front] == quadruplet[2]) ++front;
-                    
-                        // Processing the duplicates of number 4
-                        while (front < back && num[back] == quadruplet[3]) --back;
-                
+    void increaseleft(int &k, int l, vector<int> nums)
+    {
+        while(k < l && nums[k] == nums[k+1])
+            k++;
+    }
+    void decreaseright(int k, int &l, vector<int> nums)
+    {
+        while(k < l && nums[l] == nums[l-1])
+            l--;
+    }
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n(nums.size());
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+        for(int i=0;i<n-3;++i)
+        {
+            for(int j=i+1;j<n-2;++j)
+            {
+                int a(nums[i]), b(nums[j]);
+                int k = j+1, l = n-1;
+                while(k < l)
+                {
+                   int c(nums[k]), d(nums[l]);
+                    if(c + d < target-(a+b))
+                    {
+                        // increaseleft(k, l, nums);
+                        k++;
+                    }
+                    else if(c + d > target-(a+b))
+                    {
+                        // decreaseright(k, l, nums);
+                        l--;
+                    }
+                    else
+                    {
+                        ans.push_back(vector<int>({a, b, c, d}));
+                        increaseleft(k, l, nums);
+                        k++;
+                        decreaseright(k, l, nums);
+                        l--;
                     }
                 }
-                
-                // Processing the duplicates of number 2
-                while(j + 1 < n && num[j + 1] == num[j]) ++j;
+                while(j+1<n-2 && nums[j] == nums[j+1])
+                    j++;
             }
-        
-            // Processing the duplicates of number 1
-            while (i + 1 < n && num[i + 1] == num[i]) ++i;
-        
+            while(i+1<n-3 && nums[i] == nums[i+1])
+                i++;
         }
-    
-        return res;
+        return ans;
     }
-	
-//     vector<vector<int>> twoSum(vector<int>& nums, int target, int start) {
-//         vector<vector<int>> res;
-//         int lo = start, hi = nums.size() - 1;
-    
-//         while (lo < hi) {
-//             int curr_sum = nums[lo] + nums[hi];
-//             if (curr_sum < target || (lo > start && nums[lo] == nums[lo - 1])) {
-//                 ++lo;
-//             } else if (curr_sum > target || (hi < nums.size() - 1 && nums[hi] == nums[hi + 1])) {
-//                 --hi;
-//             } else {
-//                 res.push_back({ nums[lo++], nums[hi--] });
-//             }
-//         }
-                                                           
-//         return res;
 };
