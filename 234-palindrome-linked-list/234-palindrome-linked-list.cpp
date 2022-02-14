018b55c290f0faa89bf2b17875900efd5e0392dc
avnyaswanth <68109893+avnyaswanth@users.noreply.github.com>
2022-02-14 12:40:32 +0530

    Time: 273 ms (61.25%), Space: 117.3 MB (61.35%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -1,68 +1,43 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-#define node ListNode
-#define data val
 class Solution {
 public:
-    
-    node *reverse(node *h)
-	{
-		if(h==NULL||h->next==NULL)
-		return h;
-		node *prev = NULL,*cur = h,*after = h;
-		while(cur!=NULL)
-		{
-			after = cur->next;
-			cur->next = prev;
-			prev = cur;
-			cur = after;
-		}
-		return prev;
-	}
-	
-	bool compare(node *cur,node *midstart)
-	{
-		while(midstart!=NULL)
-		{
-			if(cur->data!=midstart->data)
-			return false;
-			cur = cur->next;
-			midstart = midstart->next;
-		}
-		return true;
-	}
-    
+    ListNode* reverse(ListNode *head)
+    {
+        if(!head || !head->next)
+            return head;
+        
+        ListNode *revHead = reverse(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        return revHead;
+            
+    }
     bool isPalindrome(ListNode* head) {
-        if(head==NULL||head->next==NULL)
-		return true;	
-		node *mid = head;
-		node *slow = head,*fast = head,*prev=head;
-		while(fast&&fast->next)
-		{
-			prev = slow;
-			slow = slow->next;
-			fast = fast->next->next;
-		}
-		node *midstart,*cur = head;;
-		if(fast!=NULL)
-		{
-			slow->next = reverse(slow->next);
-			midstart = slow->next;
-		}
-		else
-		{
-			prev->next = reverse(slow);
-			midstart = prev->next;
-		}
-		bool ans = compare(head,midstart);
-		return ans;
+        if(!head || !head->next)
+            return true;
+        
+        ListNode *slow = new ListNode();
+        slow->next = head;
+        ListNode *fast = slow;
+        while(fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        ListNode *revhead = reverse(slow->next);
+        ListNode *revstart = revhead;
+        ListNode *headstart = head;
+        // slow->next = NULL;
+
+        while(revstart)
+        {
+            if(headstart->val != revstart->val)
+                return false;
+            headstart = headstart->next;
+            revstart = revstart->next;
+        }
+        
+        slow->next = reverse(revhead);
+        return true;
     }
 };
