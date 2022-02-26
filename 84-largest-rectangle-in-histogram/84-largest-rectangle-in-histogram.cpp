9bc46db1bda75e29a619464975acc213140e8776
avnyaswanth <68109893+avnyaswanth@users.noreply.github.com>
2022-02-26 10:19:22 +0530

    Time: 136 ms (82.45%), Space: 75.3 MB (20.04%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -3,34 +3,21 @@ public:
     int largestRectangleArea(vector<int>& arr) {
         int maxArea = INT_MIN;
         int n(arr.size());
-        int leftsmall[n], rightsmall[n];
+        
         stack<int> stk;
-        for(int i=0;i<n;++i)
+        for(int i=0;i<=n;++i)
         {
-            while(!stk.empty() && arr[stk.top()] >= arr[i])
+            while(!stk.empty() && (i == n || arr[stk.top()] >= arr[i]))
+            {
+                int height = arr[stk.top()];
                 stk.pop();
-            if(stk.empty())
-                leftsmall[i] = 0;
-            else
-                leftsmall[i] = stk.top() + 1;
+                int area = 0;
+                if(stk.empty())
+                    area = i * height;
+                else area = (i - (stk.top()+1)) * height;
+                maxArea = max(maxArea, area);
+            }  
             stk.push(i);
-        }
-        
-        while(!stk.empty())
-            stk.pop();
-        
-        for(int i=n-1;i>=0;--i)
-        {
-            while(!stk.empty() && arr[stk.top()] >= arr[i])
-                stk.pop();
-            if(stk.empty())
-                rightsmall[i] = n-1;
-            else rightsmall[i] = stk.top() - 1;
-            stk.push(i);
-        }
-        for(int i=0;i<n;++i)
-        {
-            maxArea = max(maxArea, (rightsmall[i] - leftsmall[i] + 1) * arr[i]);
         }
         return maxArea;
     }
