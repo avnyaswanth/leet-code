9c9f707c1aa4fdbf0f019bb3218cfe87dc6be268
avnyaswanth <68109893+avnyaswanth@users.noreply.github.com>
2022-03-10 13:01:34 +0530

    Time: 290 ms (38.01%), Space: 16.7 MB (51.09%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     int dp[10001];
-    int jump(int idx, vector<int>& nums, int n)
-    {
-        if(idx >= n-1)
-            return 0;
-
-        if(dp[idx] != 10001)
-            return dp[idx];
-        
-        for(int i=1;i<=nums[idx];++i)
-            dp[idx] = min(dp[idx], 1 + jump(i+idx, nums, n));
-        
-        return dp[idx];
-    }
     
     int jump(vector<int>& nums) {
-        int n(nums.size());
         
+        int n(nums.size());
         for(int i=0;i<10001;++i)
             dp[i] = 10001;
         
-        int ans = jump(0, nums, n);
-        return ans;
+        dp[n-1] = 0;
+        
+        for(int i=n-2;i>=0;--i)
+        {
+            for(int j=1;j<=nums[i] && i+j < n;++j)
+            {
+                dp[i] = min(dp[i], 1 + dp[i+j]);
+            }
+        }
+        
+        return dp[0];
     }
 };
