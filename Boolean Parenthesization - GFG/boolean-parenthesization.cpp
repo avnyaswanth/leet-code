3acc57724d5f55c9a3590b03cd61dc733ef4b9a6
avnyaswanth <68109893+avnyaswanth@users.noreply.github.com>
2022-03-13 09:43:47 +0530

    Added solution - LeetHub

--- Boolean Parenthesization - GFG/boolean-parenthesization.cpp
+++ Boolean Parenthesization - GFG/boolean-parenthesization.cpp
@@ -9,128 +9,63 @@ using namespace std;
 
 class Solution{
 public:
-    unordered_map<string,int> mp;
+    unordered_map<string, int> mp;
     
-    int solve(string S,int i,int j, bool isTrue)
+    int countways(string s, int n, int i, int j, bool istrue)
     {
-        if(i > j) return 0;
-        if(i == j) {
-            if(isTrue) {
-                return S[i] == 'T' ? 1 : 0;
-            }
-            else {
-                return S[i] == 'F' ? 1 : 0;
-            }
+        if(i > j)
+            return 0;
+            
+        if(i == j)
+        {
+            if(istrue == true)
+                return s[i] == 'T';
+            else return s[i] == 'F';
         }
         
         string key = to_string(i);
-        key.push_back(' ');
-        key.append(to_string(j));
-        key.push_back(' ');
-        key.append(to_string(isTrue));
-        
-        if(mp.find(key) != mp.end()) {
+        key += " " + to_string(j) + " " + to_string(istrue);
+        if(mp.find(key) != mp.end())
             return mp[key];
-        }
         
         int ans = 0;
         
-        for(int k = i+1; k<=j-1; k=k+2)
+        for(int k=i+1;k<=j-1;k += 2)
         {
-            int leftTrue, leftFalse, rightTrue, rightFalse;
+            int lt = countways(s, n, i, k-1, true);
+            int lf = countways(s, n, i, k-1, false);
+            int rt = countways(s, n, k+1, j, true);
+            int rf = countways(s, n, k+1, j, false);
             
-            string leftTruekey = to_string(i);
-            leftTruekey.push_back(' '); 
-            leftTruekey.append(to_string(k-1)); 
-            leftTruekey.push_back(' '); 
-            leftTruekey.append(to_string(true));
-            
-            string leftFalsekey = to_string(i);
-            leftFalsekey.push_back(' '); 
-            leftFalsekey.append(to_string(k-1)); 
-            leftFalsekey.push_back(' '); 
-            leftFalsekey.append(to_string(false));
-            
-            string rightTruekey = to_string(k+1); 
-            rightTruekey.push_back(' '); 
-            rightTruekey.append(to_string(j)); 
-            rightTruekey.push_back(' '); 
-            rightTruekey.append(to_string(true));
-            
-            string rightFalsekey = to_string(k+1); 
-            rightFalsekey.push_back(' '); 
-            rightFalsekey.append(to_string(j)); 
-            rightFalsekey.push_back(' ');
-            rightFalsekey.append(to_string(false));
-            
-            if(mp.find(leftTruekey) != mp.end()) {
-                leftTrue = mp[leftTruekey];
-            }
-            else {
-                leftTrue = solve(S,i,k-1,true);
-                mp[leftTruekey] = leftTrue;
-            }
-            
-            if(mp.find(leftFalsekey) != mp.end()) {
-                leftFalse = mp[leftFalsekey];
-            }
-            else {
-               leftFalse = solve(S,i,k-1,false);
-               mp[leftFalsekey] = leftFalse;
-            }
-            
-            if(mp.find(rightTruekey) != mp.end()) {
-                rightTrue = mp[rightTruekey];
-            }
-            else {
-                rightTrue = solve(S,k+1,j,true);
-                mp[rightTruekey] = rightTrue;
-            }
-            
-            if(mp.find(rightFalsekey) != mp.end()) {
-                rightFalse = mp[rightFalsekey];
-            }
-            else {
-                rightFalse = solve(S,k+1,j,false);
-                mp[rightFalsekey] = rightFalse;
-            }
-            
-            if(S[k] == '&') 
+            if(s[k] == '&')
             {
-                if(isTrue) {
-                    ans = ans + leftTrue*rightTrue; 
-                }
-                else {
-                    ans = ans + leftFalse*rightTrue + leftTrue*rightFalse + leftFalse*rightFalse;  
-                }
-            }
-            else if(S[k] == '|') 
-            {
-                if(isTrue) {
-                    ans = ans + leftTrue*rightFalse + leftFalse*rightTrue + leftTrue*rightTrue;
-                }
-                else {
-                    ans = ans + leftFalse*rightFalse;  
-                }
-            }
-            else // S[k] == "^" 
-            {
-                if(isTrue) {
-                    ans = ans + leftFalse*rightTrue + leftTrue*rightFalse; 
-                }
-                else {
-                    ans = ans + leftTrue*rightTrue + leftFalse*rightFalse;  
-                }
+                if(istrue == true)
+                    ans += lt * rt;
+                else ans += lt * rf + lf * rt + lf * rf;
             }
             
+            else if(s[k] == '|')
+            {
+                if(istrue == true)
+                    ans += lt * rf + lf * rt + lt * rt;
+                else ans += lf * rf;
+            }
+            
+            else
+            {
+                if(istrue == true)
+                    ans += lt * rf + lf * rt;
+                else ans += lf * rf + lt * rt;
+            }
         }
-        return mp[key] = ans%1003;
+        
+        return mp[key] = ans % 1003;
     }
     
     int countWays(int N, string S){
         // code here
         mp.clear();
-        return solve(S,0,N-1,true);
+        return countways(S, N, 0,N-1,true);
     }
 };
 
